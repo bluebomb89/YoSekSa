e144c18bb299edf2f87b530e8df718ab073a1214
Park-Taejin <ptj0913@naver.com>
2015-10-21 13:46:56 +0900

    회원가입폼 이름변경, memberdao,dto생성

--- projectYoseksa/src/com/yoseksa/yoseksa.java
+++ projectYoseksa/src/com/yoseksa/yoseksa.java
@@ -5,7 +5,7 @@ public class yoseksa {
 			"index.jsp",
 			"../calendar/typo.html",//캘린더
 			"../freeboard/list.jsp",//프리보드
-			"../member/indexggong.jsp",//멤버
+			"../member/join.jsp",//멤버
 			"../recipe/gallery.jsp",//레시피
 			"../freeboard/contact.jsp"//자게 글쓰기
 	};
