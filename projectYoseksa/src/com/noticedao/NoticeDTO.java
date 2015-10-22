4d9d0e260c578ab7ac48c95e26eb4756d85e1940
youngkwon nam <bluebomb89@naver.com>
2015-10-22 18:13:16 +0900

    노티스 변경

--- projectYoseksa/src/com/noticedao/NoticeDTO.java
+++ projectYoseksa/src/com/noticedao/NoticeDTO.java
@@ -1,7 +1,47 @@
 package com.noticedao;
 
 public class NoticeDTO {
-	public static void main(String[] args){
-		System.out.println("메인지우고 DAO,DTO 코딩");
+	private int noticeno;
+	private String noticesub;
+	private int noticehire;
+	private int noticehit;
+	private String noticecont;
+	private int boardno;
+	public int getNoticeno() {
+		return noticeno;
 	}
+	public void setNoticeno(int noticeno) {
+		this.noticeno = noticeno;
+	}
+	public String getNoticesub() {
+		return noticesub;
+	}
+	public void setNoticesub(String noticesub) {
+		this.noticesub = noticesub;
+	}
+	public int getNoticehire() {
+		return noticehire;
+	}
+	public void setNoticehire(int noticehire) {
+		this.noticehire = noticehire;
+	}
+	public int getNoticehit() {
+		return noticehit;
+	}
+	public void setNoticehit(int noticehit) {
+		this.noticehit = noticehit;
+	}
+	public String getNoticecont() {
+		return noticecont;
+	}
+	public void setNoticecont(String noticecont) {
+		this.noticecont = noticecont;
+	}
+	public int getBoardno() {
+		return boardno;
+	}
+	public void setBoardno(int boardno) {
+		this.boardno = boardno;
+	}
+	
 }
