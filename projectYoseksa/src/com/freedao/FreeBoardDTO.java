1125e5770d2c4c22a770b1fd4ee269fc1c3ee24f
Park-Taejin <ptj0913@naver.com>
2015-10-23 13:57:52 +0900

    프리보드 dto수정

--- projectYoseksa/src/com/freedao/FreeBoardDTO.java
+++ projectYoseksa/src/com/freedao/FreeBoardDTO.java
@@ -1,30 +1,42 @@
 package com.freedao;
 import java.util.*;
 public class FreeBoardDTO {
-	private int free_no;
 	private int board_no;
+	private int free_no;
 	private int member_no;
+	private String nickname;
+	private String member_pic;
 	private String free_content;
 	private Date free_date;
-	private int free_hit;
-	
-	public int getFree_no() {
-		return free_no;
-	}
-	public void setFree_no(int free_no) {
-		this.free_no = free_no;
-	}
 	public int getBoard_no() {
 		return board_no;
 	}
 	public void setBoard_no(int board_no) {
 		this.board_no = board_no;
 	}
+	public int getFree_no() {
+		return free_no;
+	}
+	public void setFree_no(int free_no) {
+		this.free_no = free_no;
+	}
 	public int getMember_no() {
 		return member_no;
 	}
 	public void setMember_no(int member_no) {
 		this.member_no = member_no;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getMember_pic() {
+		return member_pic;
+	}
+	public void setMember_pic(String member_pic) {
+		this.member_pic = member_pic;
 	}
 	public String getFree_content() {
 		return free_content;
@@ -38,10 +50,5 @@ public class FreeBoardDTO {
 	public void setFree_date(Date free_date) {
 		this.free_date = free_date;
 	}
-	public int getFree_hit() {
-		return free_hit;
-	}
-	public void setFree_hit(int free_hit) {
-		this.free_hit = free_hit;
-	}
+
 }
