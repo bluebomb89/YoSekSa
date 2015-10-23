762918bdeeb89d44ad99916646f6a6db5ecf36f9
youngkwon nam <bluebomb89@naver.com>
2015-10-23 12:55:44 +0900

    freeboarddto충돌 수정

--- projectYoseksa/src/com/freedao/FreeBoardDTO.java
+++ projectYoseksa/src/com/freedao/FreeBoardDTO.java
@@ -1,7 +1,32 @@
 package com.freedao;
-
+import java.util.*;
 public class FreeBoardDTO {
-	public static void main(String[] args){
-		System.out.println("메인지우고 DAO,DTO 코딩");
+	private int free_no;
+	private String free_content;
+	private Date free_date;
+	private int free_hit;
+	public int getFree_no() {
+		return free_no;
+	}
+	public void setFree_no(int free_no) {
+		this.free_no = free_no;
+	}
+	public String getFree_content() {
+		return free_content;
+	}
+	public void setFree_content(String free_content) {
+		this.free_content = free_content;
+	}
+	public Date getFree_date() {
+		return free_date;
+	}
+	public void setFree_date(Date free_date) {
+		this.free_date = free_date;
+	}
+	public int getFree_hit() {
+		return free_hit;
+	}
+	public void setFree_hit(int free_hit) {
+		this.free_hit = free_hit;
 	}
 }
