3d2990dfc129a986f271a82a0684d147c0ecb38a
minyeong kim <iqeeiqee2@nate.com>
2015-10-26 18:05:41 +0900

    게시판

--- projectYoseksa/src/com/freedao/FreeBoardDTO.java
+++ projectYoseksa/src/com/freedao/FreeBoardDTO.java
@@ -1,7 +1,57 @@
 package com.freedao;
 
+import java.util.*;
+
 public class FreeBoardDTO {
-	public static void main(String[] args){
-		System.out.println("메인지우고 DAO,DTO 코딩");
+	private int free_no;
+	private int board_no;
+	private int member_no;
+	private String free_content;
+	private Date free_date;
+	private int free_hit;
+	private String free_pic;
+	
+	public int getFree_no() {
+		return free_no;
 	}
+	public void setFree_no(int free_no) {
+		this.free_no = free_no;
+	}
+	public int getBoard_no() {
+		return board_no;
+	}
+	public void setBoard_no(int board_no) {
+		this.board_no = board_no;
+	}
+	public int getMember_no() {
+		return member_no;
+	}
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+	public String getFree_content() {
+		return free_content;
+	}
+	public void setFree_content(String free_content) {
+		this.free_content = free_content;
+	}
+	public Date getFree_date() {
+		return free_date;
+	}
+	public void setFree_date(Date free_date) {
+		this.free_date = free_date;
+	}
+	public int getFree_hit() {
+		return free_hit;
+	}
+	public void setFree_hit(int free_hit) {
+		this.free_hit = free_hit;
+	}
+	public String getFree_pic() {
+		return free_pic;
+	}
+	public void setFree_pic(String free_pic) {
+		this.free_pic = free_pic;
+	}
+	
 }
