30a56a124ab720bb4e96c0af93f1344c975250f7
hongeui choi <hongeui90@gmail.com>
2015-10-29 11:38:48 +0900

    notice(DAO,Mapper,Model)수정

--- projectYoseksa/src/com/noticedao/NoticeDAO.java
+++ projectYoseksa/src/com/noticedao/NoticeDAO.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.*;
 import org.apache.ibatis.io.*;
 import org.apache.ibatis.session.*;
 
+import com.noticedao.NoticeDTO;
+
 public class NoticeDAO {
 	private static SqlSessionFactory ssf;
 	static{
@@ -17,7 +19,28 @@ public class NoticeDAO {
 			System.out.println(ex.getMessage());
 		}
 	}
-	public List<NoticeDTO> empAllData(){
-		return ssf.openSession().selectList("empAllData");
+	public static List<NoticeDTO> noticeListData()
+	{
+		List<NoticeDTO> list=new ArrayList<NoticeDTO>();
+		SqlSession session=null;
+		try
+		{
+			session=ssf.openSession();
+			list=session.selectList("NoticeDTO");
+		}catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		finally
+		{
+			if(session!=null)
+				session.close();
+		}
+		return list;
+		//return ssf.openSession().selectList("boardListData",map);
 	}
+	
+	/*public List<NoticeDTO> empAllData(){
+		return ssf.openSession().selectList("empAllData");
+	}*/
 }
