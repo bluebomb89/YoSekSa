9c6207c7d66bfa67f875393d26129282ced1b46c
youngkwon nam <bluebomb89@naver.com>
2015-10-30 11:19:21 +0900

    공지사항 목록 출력

--- projectYoseksa/src/com/noticedao/NoticeDTO.java
+++ projectYoseksa/src/com/noticedao/NoticeDTO.java
@@ -9,6 +9,13 @@ public class NoticeDTO {
 	private String notice_content;
 	private Date notice_date;
 	private int notice_hit;
+	private Date notice_dbday;
+	public Date getNotice_dbday() {
+		return notice_dbday;
+	}
+	public void setNotice_dbday(Date notice_dbday) {
+		this.notice_dbday = notice_dbday;
+	}
 	BoardmgrDTO board=new BoardmgrDTO();
 	MemberDTO member=new MemberDTO();
 	
