db42f5b54eef4872422c4801b3ba017b3363190a
youngkwon nam <bluebomb89@naver.com>
2015-11-03 08:49:47 +0900

    레시피서치 예외처리 추가

--- projectYoseksa/src/com/model/MainView.java
+++ projectYoseksa/src/com/model/MainView.java
@@ -1,6 +1,7 @@
 package com.model;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -47,9 +48,8 @@ public class MainView {
 		return "yoSeksa/function/main/main.jsp";
 	}
 	@RequestMapping("recipe_search.sek")
-	public String recipe_search(HttpServletRequest req) throws IOException{
+	public String recipe_search(HttpServletRequest req) throws IOException {
 		req.setCharacterEncoding("EUC-KR");
-		System.out.println("sss");
 		Elements elements;
 		Elements elementsimg;
 		Elements elementstitle;
@@ -57,80 +57,92 @@ public class MainView {
 		List<String> titlelist=new ArrayList<String>();
 		List<String> linklist=new ArrayList<String>();
 		int a=0;
-	    	int page=0;
-	    	String searchname = req.getParameter("mangae-search");
-	    	String mspage=req.getParameter("mspage").trim();
-	    	System.out.println("서치네임"+searchname);
-	    	
-	    	int nconfirm;
-	        Document document;
-	        if(mspage==null){
-	        	mspage="1";
-	        }else if(mspage==""){
-	        	mspage="1";
-	        }
-	        if(searchname==null){
-	        	System.out.println("널?");
-	        	searchname="";
-	        } else if(searchname==""){
-	        	searchname="";
-	        }
-	        System.out.println("mspag="+mspage);
-	        if(Integer.parseInt(mspage)%2==0){
-	        	page=(Integer.parseInt(mspage)/2);
-	        	nconfirm=1;
-	        }else{
-	        	page=(Integer.parseInt(mspage)/2)+1;
-	        	nconfirm=0;
+    	int page=0;
+    	// 검색을 누르면 searchname에 검색값을 입력한다.
+    	String searchname = req.getParameter("mangae-search");
+    	// 페이지값을 받아온다
+    	String mspage=req.getParameter("mspage").trim();
+    	int nconfirm;
+    	// page뒤에 숫자가 게시판 넘버. 
+        Document document;
+        if(mspage==null){
+        	mspage="1";
+        }else if(mspage==""){
+        	mspage="1";
+        }
+        // 한페이지에 20개의 데이타. 우리에겐 10개씩 보내니까. 2로나눈 나머지가 0이아니면 몫을 1증가시켜주면 된다
+        // mspage는 버튼값을 넘기기 위해 만들고 page는 그냥 보여주기로.
+        if(Integer.parseInt(mspage)%2==0){
+        	page=(Integer.parseInt(mspage)/2);
+        	nconfirm=1;
+        }else{
+        	page=(Integer.parseInt(mspage)/2)+1;
+        	nconfirm=0;
 
-	        }
-	        System.out.println(mspage);
-	    	
-	        String mangaesearch =  URLEncoder.encode(searchname, "UTF-8");
-			document = Jsoup.connect("http://www.10000recipe.com/recipe/list.html?q="+mangaesearch+"&order=weight&page="+page).get();
-	        if (null!=document) {
-	        	elements = document.select("a.thumbnail");
-	        	elementsimg = document.select("a.thumbnail > img[src$=.jpg]");
-//	        	System.out.println("elementsimg: "+elementsimg);
-	        	elementstitle = document.select("a.thumbnail > div > h4");	        	
-//	        	System.out.println("elementstitle: "+elementstitle);
-	            for (int i = 0; i < elements.size(); i++) {
-	            	if(i<=elements.size()-1){          		
-			            	a++;
-			            	String mangaeimg=elementsimg.get(i).attr("src");
-			            	String mangaetitle=elementstitle.get(i).text();
-			            	String mangaelink="http://www.10000recipe.com"+ elements.get(i).attr("href");
+        }
+        System.out.println(mspage);
+    	
+        String mangaesearch =  URLEncoder.encode(searchname, "UTF-8");
+        System.out.println("1");
+		document = Jsoup.connect("http://www.10000recipe.com/recipe/list.html?q="+mangaesearch+"&order=weight&page="+page).get();
+		System.out.println("2");
+        if (null!=document) {
+        	System.out.println("3");
+        	// a태그의 클래스가 thumbnail인 모든값을 가져옵니다.
+        	elements = document.select("a.thumbnail");
+        	System.out.println(elements);
+        	elementsimg = document.select("a.thumbnail > img[src$=.jpg]");
+	        	System.out.println("elementsimg: "+elementsimg);
+        	elementstitle = document.select("a.thumbnail > div > h4");	        	
+	        	System.out.println("elementstitle: "+elementstitle);
+	        	System.out.println("사이즈"+elements.size());
+            for (int i = 0; i < elements.size(); i++) {
+            	if(i<=elements.size()-1){
+            		try{
+            			System.out.println("i="+i);
+		            	a++;
+		            	String mangaeimg=elementsimg.get(i).attr("src");
+		            	String mangaetitle=elementstitle.get(i).text();
+		            	String mangaelink="http://www.10000recipe.com"+ elements.get(i).attr("href");
+
+		            	System.out.println("mangaeimg="+mangaeimg);
+		            	System.out.println("mangaelise="+titlelist);
+		            	System.out.println("mangaelink="+mangaelink);
 //			                System.out.println("------------------------------------------");
-//			                System.out.println("��� ��ȣ :" + a);
-//			                System.out.println("�˻��� : " + elementstitle.get(i).text());
-//			                System.out.println("���� : " + mangaetitle);
-//			                System.out.println("��ũ URL : " +"http://www.10000recipe.com"+ elements.get(i).attr("href"));
-//			                System.out.println("��ũ URL : " + mangaelink);
-//			                System.out.println("�̹��� src : " + mangaeimg);
+//			                System.out.println("출력 번호 :" + a);
+//			                System.out.println("검색어 : " + elementstitle.get(i).text());
+//			                System.out.println("제목 : " + mangaetitle);
+//			                System.out.println("링크 URL : " +"http://www.10000recipe.com"+ elements.get(i).attr("href"));
+//			                System.out.println("링크 URL : " + mangaelink);
+//			                System.out.println("이미지 src : " + mangaeimg);
 //			                System.out.println("------------------------------------------");
-			                // Ŭ���ϸ� ��ũURL �����±׸� �о �����Է�!
-			            	imglist.add(mangaeimg);
-			            	titlelist.add(mangaetitle);
-			            	linklist.add(mangaelink);
-			            	
-			        	}
-	            	} //System.out.println("����� ��="+elements.size());
-	            }
-	        System.out.println("짝수? 홀수?"+nconfirm);
-			req.setAttribute("imglist", imglist);
-			req.setAttribute("titlelist", titlelist);
-			req.setAttribute("linklist", linklist);
-			req.setAttribute("nconfirm", nconfirm);
-			req.setAttribute("mspage", mspage);
-			return "yoSeksa/function/main/mangae.jsp";
+		                // 클릭하면 링크URL 내용태그를 읽어서 나에게로!
+
+		            	imglist.add(mangaeimg);
+		            	titlelist.add(mangaetitle);
+		            	linklist.add(mangaelink);
+            		}catch(Exception ex){
+            			break;
+            		}
+		        	}
+            	} //System.out.println("저장된 값="+elements.size());
+            }
+//	        System.out.println("짝수? 홀수?"+nconfirm);
+		System.out.println("레시피서치 메소드 끝");
+		req.setAttribute("imglist", imglist);
+		req.setAttribute("titlelist", titlelist);
+		req.setAttribute("linklist", linklist);
+		req.setAttribute("nconfirm", nconfirm);
+		req.setAttribute("mspage", mspage);
+		return "yoSeksa/function/main/mangae.jsp";
 	}
 	@RequestMapping("recipeview.sek")
 	public String recipeview(HttpServletRequest req) throws IOException{
 //    	String url="http://www.10000recipe.com"+rm.elements.get(14).attr("href");
-//    	System.out.println("���˿�"+url);
+//    	System.out.println("유알엘"+url);
 //    	System.out.println("??"+rm.elements);
-//    	 �����Ǹ����� �����ؼ� �����Ǹ������� Ŭ���� url�� �������� ������url�ּҿ� http://www.10000recipe.com�� �ٿ���
-//    	 document�� �ְ� document���� html�� �м� & �Ľ��Ѵ�.
+//    	 레시피만개와 연동해서 레시피만개에서 클릭시 url을 가져오고 가져온url주소에 http://www.10000recipe.com을 붙여서
+//    	 document에 넣고 document에서 html을 분석 & 파싱한다.
 		String link=req.getParameter("link");
 		String img=req.getParameter("img");
 		String title=req.getParameter("title");
@@ -156,7 +168,7 @@ public class MainView {
 		req.setAttribute("img", img);
 		req.setAttribute("title", title);
 		req.setAttribute("searchview", searchview);
-		return "yoSeksa/function/main/main.jsp";		
+		return "yoSeksa/function/main/main.jsp";
 	}
 	
 }
