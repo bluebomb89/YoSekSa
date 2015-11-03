7d0832e6fead49045c3807ea4ff6b354a75602e3
seunghyun lee <phic901@naver.com>
2015-11-03 11:41:55 +0900

    켈린더 래시피 팝업 클릭시 닫을 수 있게 수정

--- projectYoseksa/src/com/controller/FileConfig.java
+++ projectYoseksa/src/com/controller/FileConfig.java
@@ -5,7 +5,8 @@ public class FileConfig {
 	public List<String> getJavaFile(String pack){
 		List<String> list=new ArrayList<String>();
 		try{
-			String path="C:\\Users\\남영권\\git\\yoseksaProject\\projectYoseksa\\src\\";
+			/*String path="C:\\Users\\�궓�쁺沅�\\git\\yoseksaProject\\projectYoseksa\\src\\";*/
+			String path="C:\\Users\\sist\\git\\YoSekSa\\projectYoseksa\\src\\";
 			String temp=pack.replace('.', '\\');
 			File dir=new File(path+temp);
 			File[] files=dir.listFiles();
