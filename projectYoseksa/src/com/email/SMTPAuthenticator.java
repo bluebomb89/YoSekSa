66da2faffc5f3e3252c599dd110471d125fc9971
minyeong kim <iqeeiqee2@nate.com>
2015-11-06 11:46:46 +0900

    asdf

---
+++ projectYoseksa/src/com/email/SMTPAuthenticator.java
@@ -0,0 +1,11 @@
+package com.email;
+
+import javax.mail.PasswordAuthentication;
+
+public class SMTPAuthenticator extends javax.mail.Authenticator {
+    public PasswordAuthentication getPasswordAuthentication() {
+    	// 네이버나 Gmail 사용자 계정 설정.
+        // Gmail의 경우 @gmail.com을 제외한 아이디만 입력한다.
+        return new PasswordAuthentication("iqee9932", "1a3a5a");
+    }
+}
