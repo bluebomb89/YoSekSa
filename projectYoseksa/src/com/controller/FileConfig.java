e1bc34ba390904101de270f4745397392625f877
seunghyun lee <phic901@naver.com>
2015-11-10 08:59:48 +0900

    wererwe

--- projectYoseksa/src/com/controller/FileConfig.java
+++ projectYoseksa/src/com/controller/FileConfig.java
@@ -7,7 +7,7 @@ public class FileConfig {
       List<String> list=new ArrayList<String>();
       try{
 //       영권경로
-        String path="C:\\Users\\남영권\\git\\yoseksaProject\\projectYoseksa\\src\\";
+//        String path="C:\\Users\\남영권\\git\\yoseksaProject\\projectYoseksa\\src\\";
 //       민영경로
 //         String path="C:\\Users\\sist\\git\\YoSekSa\\projectYoseksa\\src\\";
 //        홍의경로
@@ -17,7 +17,7 @@ public class FileConfig {
 //         String path="C:\\Users\\sist\\git\\YoSekSa\\projectYoseksa\\src\\";
 //         String path="C:\\Users\\ssd\\git\\yoseksaProject\\projectYoseksa\\src\\";
 //        승현경로
-//         String path="C:\\Users\\sist\\git\\YoSekSa\\projectYoseksa\\src\\";
+         String path="C:\\Users\\sist\\git\\YoSekSa\\projectYoseksa\\src\\";
 //         String path="C:\\Users\\sist\\git\\YoSekSa\\projectYoseksa\\src\\";
 //        태진경로
 //         String path="C:\\Users\\sist\\git\\YoSekSa\\projectYoseksa\\src\\";
