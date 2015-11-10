95205fa4365400baea804e9cd4578cec9a47d55f
hongeui choi <hongeui90@gmail.com>
2015-11-10 14:59:14 +0900

    ww

--- projectYoseksa/src/com/controller/FileConfig.java
+++ projectYoseksa/src/com/controller/FileConfig.java
@@ -22,7 +22,7 @@ public class FileConfig {
 //        태진경로
 //         String path="C:\\Users\\sist\\git\\YoSekSa\\projectYoseksa\\src\\";
 //        우식경로
-         String path="C:\\Users\\sist-77\\git\\YoSekSa\\projectYoseksa\\src\\";
+//        String path="C:\\Users\\sist-77\\git\\YoSekSa\\projectYoseksa\\src\\";
 
          String temp=pack.replace('.', '\\');
          File dir=new File(path+temp);
